7dc6c61bdf72ef52e1b1a13aa561de76d763f1fc
Hyunsun Moon <hyunsun.moon@gmail.com>
2016-05-24 18:38:40 -0400

    Added sudo

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker rm -v $(docker ps -a -q -f status=exited)
-docker rmi $(docker images -f "dangling=true" -q)
-docker run -v /var/run/docker.sock:/var/run/docker.sock -v 
-/var/lib/docker:/var/lib/docker --rm martin/docker-cleanup-volumes
+sudo docker rm -v $(docker ps -a -q -f status=exited)
+sudo docker rmi $(docker images -f "dangling=true" -q)
+sudo docker run -v /var/run/docker.sock:/var/run/docker.sock -v 
+sudo /var/lib/docker:/var/lib/docker --rm martin/docker-cleanup-volumes
--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ if [[ -n "$1" ]]; then
     CONTAINER_HOSTNAME=$1
 fi
 
-docker network rm net-router
-docker network create --subnet=172.18.0.0/16 net-router
-docker run --privileged --name ${CONTAINER_HOSTNAME:-router} --hostname ${CONTAINER_HOSTNAME:-router} --net net-router --ip=172.18.0.100 -d -v ~/docker-quagga/volumes/quagga:/etc/quagga quagga
+sudo docker network rm net-router
+sudo docker network create --subnet=172.18.0.0/16 net-router
+sudo docker run --privileged --name ${CONTAINER_HOSTNAME:-router} --hostname ${CONTAINER_HOSTNAME:-router} --net net-router --ip=172.18.0.101 -d -v ~/docker-quagga/volumes/quagga:/etc/quagga quagga
