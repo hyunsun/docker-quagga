556a6156f7868ba3d2494fdf124a01951ec55edd
hyunsun <hyunsun.moon@gmail.com>
2016-05-24 21:08:39 -0400

    Use pipework to bring up a container

--- run.sh
+++ run.sh
@@ -4,6 +4,8 @@ if [[ -n "$1" ]]; then
     CONTAINER_HOSTNAME=$1
 fi
 
-sudo docker network rm net-router
-sudo docker network create --subnet=172.18.0.0/16 net-router
-sudo docker run --privileged --name ${CONTAINER_HOSTNAME:-router} --hostname ${CONTAINER_HOSTNAME:-router} --net net-router --ip=172.18.0.101 -d -v ~/docker-quagga/volumes/quagga:/etc/quagga quagga
+if [[ -n "$2" ]]; then
+    CONTAINER_IP=$2
+fi
+
+./pipework ovsbr0 -i eth0 $(docker run --net='none' --privileged --name ${CONTAINER_HOSTNAME:-router} --hostname ${CONTAINER_HOSTNAME:-router} -d -v ~/docker-quagga/volumes/quagga:/etc/quagga quagga) ${CONTAINER_IP:-192.168.100.254/24}
