8d8b3bbe5ce072e12ef8db13466abe65201caa72
Hyunsun Moon <hyunsun.moon@gmail.com>
2016-05-24 21:25:25 -0400

    Added sudo before docker

--- run.sh
+++ run.sh
@@ -8,4 +8,5 @@ if [[ -n "$2" ]]; then
     CONTAINER_IP=$2
 fi
 
-./pipework ovsbr0 -i eth0 $(docker run --net='none' --privileged --name ${CONTAINER_HOSTNAME:-router} --hostname ${CONTAINER_HOSTNAME:-router} -d -v ~/docker-quagga/volumes/quagga:/etc/quagga quagga) ${CONTAINER_IP:-192.168.100.254/24}
+sudo docker run --net='none' --privileged --name ${CONTAINER_HOSTNAME:-router} --hostname ${CONTAINER_HOSTNAME:-router} -d -v ~/docker-quagga/volumes/quagga:/etc/quagga quagga
+sudo ~/docker-quagga/pipework ovsbr0 -i eth0 ${CONTAINER_HOSTNAME:-router} ${CONTAINER_IP:-192.168.100.254/24}
