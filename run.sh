2b86a8d39262f1714ee98415b04df10e27553f53
hyunsun <hyunsun.moon@gmail.com>
2016-05-24 22:23:16 -0400

    Added run.sh arguments check and set router id automatically in bgpd.conf

--- run.sh
+++ run.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
-if [[ -n "$1" ]]; then
-    CONTAINER_HOSTNAME=$1
+if [ -z "$1" ]
+  then
+    echo "Container hostname is required"
+    echo "Usage: run.sh [hostname] [IP address]"
+    exit 0
 fi
 
-if [[ -n "$2" ]]; then
-    CONTAINER_IP=$2
+if [ -z "$2" ]
+  then
+    echo "Container IP address is required"
+    echo "Usage: run.sh [hostname] [IP address]"
+    exit 0
 fi
 
-sudo docker run --net='none' --privileged --name ${CONTAINER_HOSTNAME:-router} --hostname ${CONTAINER_HOSTNAME:-router} -d -v ~/docker-quagga/volumes/quagga:/etc/quagga quagga
-sudo ~/docker-quagga/pipework ovsbr0 -i eth0 ${CONTAINER_HOSTNAME:-router} ${CONTAINER_IP:-192.168.100.254/24}
+CONTAINER_HOSTNAME=$1
+CONTAINER_IP_CIDR=$2
+CONTAINER_IP=$(echo $CONTAINER_IP_CIDR | cut -d'/' -f 1)
+
+cp ~/docker-quagga/volumes/quagga/bgpd.conf.sample ~/docker-quagga/volumes/quagga/bgpd.conf
+sed -i 's/container-ip/'${CONTAINER_IP}'/g' ~/docker-quagga/volumes/quagga/bgpd.conf
+
+sudo docker run --net='none' --privileged --name $CONTAINER_HOSTNAME --hostname $CONTAINER_HOSTNAME -d -v ~/docker-quagga/volumes/quagga:/etc/quagga quagga
+sudo ~/docker-quagga/pipework br-ex -i eth0 $CONTAINER_HOSTNAME $CONTAINER_IP_CIDR
