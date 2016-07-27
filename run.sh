0a95d6c9bf34315149a3c56f73ae23c39fc18447
Hyunsun Moon <hyunsun.mooon@gmail.com>
2016-07-26 21:14:09 -0400

    Removed unnecessary options from sample run command

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 # Example command to run the container with quagga-fpm image
 # Replace VOLUME with the path where your docker configs are
 VOLUME=~/docker-quagga/volumes/quagga
-sudo docker run --privileged --cap-add=NET_ADMIN --cap-add=NET_RAW -d -v $VOLUME:/etc/quagga hyunsun/quagga-fpm
+sudo docker run --privileged -d -v $VOLUME:/etc/quagga hyunsun/quagga-fpm
