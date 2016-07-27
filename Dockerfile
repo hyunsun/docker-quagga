de06f02074ebb17cfd72233227e59559b74ebc65
Hyunsun Moon <hyunsun.mooon@gmail.com>
2016-07-26 20:45:39 -0400

    Set /etc/quagga as the config path explicitly

--- Dockerfile
+++ Dockerfile
@@ -8,10 +8,11 @@ RUN apt-get install -qy --no-install-recommends telnet build-essential gawk wget
 ADD change.diff /tmp/change.diff
 RUN wget http://download.savannah.gnu.org/releases/quagga/quagga-0.99.23.tar.gz
 RUN tar zxvf quagga-0.99.23.tar.gz
-RUN cd quagga-0.99.23 && patch -p1 < /tmp/change.diff
-RUN cd quagga-0.99.23 && ./configure --enable-fpm --prefix=/usr
-RUN cd quagga-0.99.23 && make
-RUN cd quagga-0.99.23 && make install
+WORKDIR /quagga-0.99.23
+RUN patch -p1 < /tmp/change.diff
+RUN ./configure --enable-fpm --prefix=/usr
+RUN make
+RUN make install
 
 # 179/tcp - bgp port
 # 2601/tcp - zebra management port
@@ -19,7 +20,6 @@ RUN cd quagga-0.99.23 && make install
 # 2605/tcp - bgpd management port
 EXPOSE 179 2601 2605
 
-VOLUME /etc/quagga
-
+WORKDIR /
 ADD supervisord.conf /etc/supervisor/conf.d/supervisord.conf
 ENTRYPOINT ["/usr/bin/supervisord"]
--- run.sh
+++ run.sh
@@ -1,36 +1,6 @@
 #!/bin/bash
 
-if [ -z "$1" ]
-  then
-    echo "Container hostname is required"
-    echo "Usage: run.sh [hostname] [ETH1 IP address] [ETH1 MAC address]"
-    exit 0
-fi
-
-if [ -z "$2" ]
-  then
-    echo "IP address for eth1 is required"
-    echo "Usage: run.sh [hostname] [ETH1 IP address] [ETH1 MAC address]"
-    exit 0
-fi
-
-if [ -z "$3" ]
-  then
-    echo "MAC address for eth1 is required"
-    echo "Usage: run.sh [hostname] [ETH1 IP address] [ETH1 MAC address]"
-    exit 0
-fi
-
-CONTAINER_HOSTNAME=$1
-ETH1_IP_CIDR=$2
-ETH1_MAC=$3
-BRIDGE_NAME=${4:-br-ex}
-
-# clean up existing container with same name and IP
-sudo docker stop $CONTAINER_HOSTNAME
-sudo docker rm $CONTAINER_HOSTNAME
-sudo ovs-vsctl del-port quagga
-
-sudo docker run --privileged --cap-add=NET_ADMIN --cap-add=NET_RAW --name $CONTAINER_HOSTNAME --hostname $CONTAINER_HOSTNAME -d -v ~/docker-quagga/volumes/quagga:/usr/etc hyunsun/quagga-fpm
-sudo ~/docker-quagga/pipework $BRIDGE_NAME -i eth1 -l quagga $CONTAINER_HOSTNAME $ETH1_IP_CIDR $ETH1_MAC
-sudo docker exec -d $CONTAINER_HOSTNAME iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+# Example command to run the container with quagga-fpm image
+# Replace VOLUME with the path where your docker configs are
+VOLUME=~/docker-quagga/volumes/quagga
+sudo docker run --privileged --cap-add=NET_ADMIN --cap-add=NET_RAW -d -v $VOLUME:/etc/quagga hyunsun/quagga-fpm
